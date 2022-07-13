3ac306f1455d9b64af73a7d6505154d5f1c9b4c1
Niclas Niegsch <n.niegsch@outlook.com>
2022-07-13 13:40:31 +0200

    Adjusted dropdown directive to close modal if clicked anywhere

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,15 +1,18 @@
-import { Directive, ElementRef, HostListener } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener } from "@angular/core";
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
+  // Use @HostBinding() if the toggle should be used on the Element with the Directive on it
+  // @HostBinding('class.show') isOpen: boolean = false;
   isOpen: boolean = false;
 
   constructor(private elRef: ElementRef) {}
 
-  @HostListener('click') mouseClick() {
-    this.isOpen = !this.isOpen;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+
     if (this.isOpen) {
       this.elRef.nativeElement.childNodes.forEach(child => {
         child.classList.add('show');
