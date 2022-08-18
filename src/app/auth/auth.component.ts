67433f516a957f451cd5619f234c41d6eeb0769f
Niclas Niegsch <n.niegsch@outlook.com>
2022-08-18 08:23:01 +0200

    Creating a Component Programmatically

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,10 +1,11 @@
-import { Component, ComponentFactoryResolver } from "@angular/core";
+import { Component, ComponentFactoryResolver, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
 import { Observable } from "rxjs";
 
 import { AuthResponseData, AuthService } from "./auth.service";
 import { AlertComponent } from '../shared/alert/alert.component';
+import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
 
 @Component({
   selector: 'auth-app',
@@ -14,6 +15,7 @@ export class AuthComponent {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
+  @ViewChild(PlaceholderDirective) alertHost: PlaceholderDirective;
 
   constructor(private authService: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver) {}
 
@@ -46,7 +48,7 @@ export class AuthComponent {
       errorMessage => {
         console.log(errorMessage);
         this.error = errorMessage;
-        this.showError(errorMessage);
+        this.showErrorAlert(errorMessage);
         this.isLoading = false;
       }
     );
@@ -58,9 +60,12 @@ export class AuthComponent {
     this.error = null;
   }
 
-  private showError(message: string) {
+  private showErrorAlert(message: string) {
     // const alertCmp = new AlertComponent();
-    const alterCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+    const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
 
+    hostViewContainerRef.createComponent(alertCmpFactory);
   }
 }
