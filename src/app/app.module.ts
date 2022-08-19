6cd78f97ac6536cfb4febc2bdd582737a8ed5ea0
Niclas Niegsch <n.niegsch@outlook.com>
2022-08-19 08:36:29 +0200

    Understanding the Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
@@ -11,14 +11,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AuthComponent } from './auth/auth.component';
 
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeService } from './recipes/recipe.service';
-import { DataStorageService } from './shared/data-storage.service';
-import { RecipesResolverService } from './recipes/recipes-resolver.service';
-import { AuthService } from './auth/auth.service';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { AuthGuard } from './auth/auth.guard';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 
 @NgModule({
   declarations: [
@@ -34,20 +28,8 @@ import { SharedModule } from './shared/shared.module';
     HttpClientModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
-  ],
-  providers: [
-    ShoppingListService, 
-    RecipeService, 
-    DataStorageService, 
-    RecipesResolverService, 
-    AuthService, 
-    {
-      provide: HTTP_INTERCEPTORS, 
-      useClass: AuthInterceptorService, 
-      multi: true
-    },
-    AuthGuard
+    SharedModule,
+    CoreModule
   ],
   bootstrap: [AppComponent]
 })
