865ecad4e0f18db19abe68adec2e281243803923
Niclas Niegsch <n.niegsch@outlook.com>
2022-08-19 09:59:53 +0200

    More Lazy Loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -21,8 +18,6 @@ import { AuthModule } from './auth/auth.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ShoppingListModule,
-    AuthModule,
     SharedModule,
     CoreModule
   ],
