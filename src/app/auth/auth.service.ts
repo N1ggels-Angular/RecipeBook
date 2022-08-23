7a6712647966fed9c9102510389703cfb32ff305
Niclas Niegsch <n.niegsch@outlook.com>
2022-08-23 17:57:30 +0200

    Using Environment Variables

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { BehaviorSubject, catchError, tap, throwError } from "rxjs";
 import { User } from "./user.model";
+import { environment } from "src/environments/environment";
 
 export interface AuthResponseData {
   kind: string;
@@ -23,7 +24,7 @@ export class AuthService {
 
   signUp(email: string, password: string) {
     return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD7Ai7XKAecuGOzHJO-fPOA04RZvFVxiLQ', 
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey, 
       {
         email: email,
         password: password,
@@ -39,7 +40,7 @@ export class AuthService {
 
   login(email: string, password: string): any {
     return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD7Ai7XKAecuGOzHJO-fPOA04RZvFVxiLQ',
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey,
       {
         email: email,
         password: password,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey: 'AIzaSyD7Ai7XKAecuGOzHJO-fPOA04RZvFVxiLQ'
 };
